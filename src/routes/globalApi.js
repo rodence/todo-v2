8923e10509fc3b99634f44a5fc70f0eb41e48fd6
unknown <helagabler@gmail.com>
2024-01-19 11:38:29 +0800

    todolist api routes

---
+++ src/routes/globalApi.js
@@ -0,0 +1,46 @@
+import axios from "axios";
+
+
+export const GetRoutes = async (routename) =>{
+    return await axios
+    .get(routename)
+    .then((response)=>{
+        return response
+    })
+    .catch((error)=>{
+        console.error("error", error);
+    });
+};
+
+export const PostRoutes = async (routename)=>{
+    return await axios
+    .post(routename)
+    .then((response)=>{
+        return response
+    })
+    .catch((error)=>{
+        console.error("error post request", error)
+    })
+}
+
+export const DeleteRoutes = async (routename)=>{
+    return await axios
+    .delete(routename)
+    .then((response)=>{
+        return response
+    })
+    .catch((error)=>{
+        console.error("error delete request", error);
+    });
+};
+
+export const PutRoutes = async (routename)=>{
+    return await axios
+    .put(routename)
+    .then((response)=>{
+        return response
+    })
+    .catch((error)=>{
+        console.error("error put request", error);
+    });
+};
